5b65a513f4781d9910ae031a3263903144c0f0fc
logan <logan@brainiumstudios.com>
2018-12-11 15:21:05 -0800

    lerp doesn't require Clone. fix mixup of a and b argument order

--- src/lerp.rs
+++ src/lerp.rs
@@ -2,8 +2,15 @@ use std::ops::{Add, Mul, Sub};
 
 pub fn lerp<T, U, V, F>(a: T, b: T, f: F) -> T
 where
-    T: Sub<T, Output = U> + Add<V, Output = T> + Clone,
+    for<'a> T: Sub<&'a T, Output = U>,
     U: Mul<F, Output = V>,
+    V: Add<T, Output = T>,
 {
-    b.clone() + (a - b) * f
+    (b - &a) * f + a
+}
+
+#[test]
+fn name() {
+    let x = lerp(0.0, 1.0, 0.25);
+    print!("{}", x);
 }
