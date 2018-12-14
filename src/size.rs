fa0e45c453587b068cf3fd815b5d2bc099667974
Jonathan Rodgers <jonathan@brainiumstudios.com>
2018-12-14 14:34:13 -0800

    implemented  mul, div, rem

--- src/size.rs
+++ src/size.rs
@@ -1,5 +1,5 @@
 use crate::vector::Vector;
-use std::ops::Add;
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Rem, RemAssign};
 
 #[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Size<T> {
@@ -28,5 +28,56 @@ impl<T: Add<RHS, Output = Output>, RHS, Output> Add<Size<RHS>> for Size<T> {
         Size {
             vector: self.vector + rhs.vector,
         }
+    }
+}
+
+impl<T: AddAssign<RHS>, RHS> AddAssign<Size<RHS>> for Size<T> {
+    fn add_assign(&mut self, rhs: Size<RHS>) {
+        self.vector += rhs.vector
+    }
+}
+
+impl<T: Mul<RHS, Output = Output>, RHS: Copy, Output> Mul<RHS> for Size<T> {
+    type Output = Size<Output>;
+    fn mul(self, rhs: RHS) -> Self::Output {
+        Size {
+            vector: self.vector * rhs,
+        }
+    }
+}
+
+impl<T: MulAssign<RHS>, RHS: Copy> MulAssign<RHS> for Size<T> {
+    fn mul_assign(&mut self, rhs: RHS) {
+        self.vector *= rhs
+    }
+}
+
+impl<T: Div<RHS, Output = Output>, RHS: Copy, Output> Div<RHS> for Size<T> {
+    type Output = Size<Output>;
+    fn div(self, rhs: RHS) -> Self::Output {
+        Size {
+            vector: self.vector / rhs,
+        }
+    }
+}
+
+impl<T: DivAssign<RHS>, RHS: Copy> DivAssign<RHS> for Size<T> {
+    fn div_assign(&mut self, rhs: RHS) {
+        self.vector /= rhs
+    }
+}
+
+impl<T: Rem<RHS, Output = Output>, RHS: Copy, Output> Rem<RHS> for Size<T> {
+    type Output = Size<Output>;
+    fn rem(self, rhs: RHS) -> Self::Output {
+        Size {
+            vector: self.vector % rhs,
+        }
+    }
+}
+
+impl<T: RemAssign<RHS>, RHS: Copy> RemAssign<RHS> for Size<T> {
+    fn rem_assign(&mut self, rhs: RHS) {
+        self.vector %= rhs
     }
 }
