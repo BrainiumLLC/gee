50008f2a6d4643536fced9702d2b9badf585011f
Francesca Plebani <francesca@brainiumstudios.com>
2019-05-10 16:41:46 -0700

    Use macros to reduce boilerplate

--- src/max.rs
+++ src/max.rs
@@ -2,86 +2,41 @@ pub trait Max {
     fn max(self, other: Self) -> Self;
 }
 
-impl Max for bool {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
-    }
-}
-impl Max for i8 {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
-    }
-}
-impl Max for i16 {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
-    }
-}
-impl Max for i32 {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
-    }
-}
-impl Max for i64 {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
-    }
-}
-impl Max for i128 {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
-    }
-}
-impl Max for isize {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
-    }
+macro_rules! impl_max {
+    ($ty:ty) => {
+        impl Max for $ty {
+            fn max(self, other: Self) -> Self {
+                std::cmp::Ord::max(self, other)
+            }
+        }
+    };
 }
 
-impl Max for u8 {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
-    }
-}
-impl Max for u16 {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
-    }
-}
-impl Max for u32 {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
-    }
-}
-impl Max for u64 {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
-    }
-}
-impl Max for u128 {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
-    }
-}
-impl Max for usize {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
-    }
-}
+impl_max!(bool);
+impl_max!(char);
+
+impl_max!(i8);
+impl_max!(i16);
+impl_max!(i32);
+impl_max!(i64);
+impl_max!(i128);
+impl_max!(isize);
+
+impl_max!(u8);
+impl_max!(u16);
+impl_max!(u32);
+impl_max!(u64);
+impl_max!(u128);
+impl_max!(usize);
 
 impl Max for f32 {
     fn max(self, other: Self) -> Self {
         f32::max(self, other)
     }
 }
+
 impl Max for f64 {
     fn max(self, other: Self) -> Self {
         f64::max(self, other)
-    }
-}
-
-impl Max for char {
-    fn max(self, other: Self) -> Self {
-        std::cmp::Ord::max(self, other)
     }
 }
--- src/min.rs
+++ src/min.rs
@@ -2,86 +2,41 @@ pub trait Min {
     fn min(self, other: Self) -> Self;
 }
 
-impl Min for bool {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
-    }
-}
-impl Min for i8 {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
-    }
-}
-impl Min for i16 {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
-    }
-}
-impl Min for i32 {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
-    }
-}
-impl Min for i64 {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
-    }
-}
-impl Min for i128 {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
-    }
-}
-impl Min for isize {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
-    }
+macro_rules! impl_min {
+    ($ty:ty) => {
+        impl Min for $ty {
+            fn min(self, other: Self) -> Self {
+                std::cmp::Ord::min(self, other)
+            }
+        }
+    };
 }
 
-impl Min for u8 {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
-    }
-}
-impl Min for u16 {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
-    }
-}
-impl Min for u32 {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
-    }
-}
-impl Min for u64 {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
-    }
-}
-impl Min for u128 {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
-    }
-}
-impl Min for usize {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
-    }
-}
+impl_min!(bool);
+impl_min!(char);
+
+impl_min!(i8);
+impl_min!(i16);
+impl_min!(i32);
+impl_min!(i64);
+impl_min!(i128);
+impl_min!(isize);
+
+impl_min!(u8);
+impl_min!(u16);
+impl_min!(u32);
+impl_min!(u64);
+impl_min!(u128);
+impl_min!(usize);
 
 impl Min for f32 {
     fn min(self, other: Self) -> Self {
         f32::min(self, other)
     }
 }
+
 impl Min for f64 {
     fn min(self, other: Self) -> Self {
         f64::min(self, other)
-    }
-}
-
-impl Min for char {
-    fn min(self, other: Self) -> Self {
-        std::cmp::Ord::min(self, other)
     }
 }
