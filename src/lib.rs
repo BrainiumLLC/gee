d3fa16e70faab603ff7f3791c32b8c4d3a2e0a20
tyler <tyler@brainiumstudios.com>
2019-08-08 12:06:44 -0700

    kill units. add some convenience methods for converting integer backed types into float backed types

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,42 @@ mod vec4;
 pub use euclid;
 
 pub use self::{
-    angle::*, circle::*, direction::*, lerp::*, mat3x2::*, mat4::*, max::*, min::*, point::*, rect::*,
-    rect_position::*, size::*, vec2::*, vec3::*, vec4::*,
+    angle::*, circle::*, direction::*, lerp::*, mat3x2::*, mat4::*, max::*, min::*, point::*,
+    rect::*, rect_position::*, size::*, vec2::*, vec3::*, vec4::*,
 };
+
+macro_rules! to_f32_f64_impl {
+    ($hkt:ident: $t:ty) => {
+        impl $hkt<$t> {
+            pub fn to_f32(self) -> $hkt<f32> {
+                self.map(|v| v as f32)
+            }
+
+            pub fn to_f64(self) -> $hkt<f64> {
+                self.map(|v| v as f64)
+            }
+        }
+    };
+    ($hkt:ident: $($t:ty),* $(,)*) => {
+        $(to_f32_f64_impl! {$hkt: $t})*
+    };
+}
+
+macro_rules! to_f32_f64 {
+    ($($hkt:ident),* $(,)*) => {
+        $(to_f32_f64_impl! {
+        $hkt:
+            i8,i16,i32,i64,i128,
+            u8,u16,u32,u64,u128,
+        })*
+    };
+}
+
+to_f32_f64! {
+    Point,
+    Rect,
+    Size,
+    Vec2,
+    Vec3,
+    Vec4,
+}
