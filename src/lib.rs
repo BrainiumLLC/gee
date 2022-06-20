6dc0ae8d65869a3fdeddcfe6c1115f9cda2a2685
Spenser Saling <spenser@brainiumstudios.com>
2022-06-20 12:25:10 -0700

    Initial split integration. There's room for improvement

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,14 @@ mod ray;
 mod rect;
 mod rect_position;
 mod size;
+mod split;
 mod transform;
 mod transform3d;
 mod vector;
 
 pub use self::{
     angle::*, circle::*, direction::*, ellipse::*, lerp::*, line_segment::*, point::*, ray::*,
-    rect::*, rect_position::*, size::*, transform::*, transform3d::*, vector::*,
+    rect::*, rect_position::*, size::*, split::*, transform::*, transform3d::*, vector::*,
 };
 pub use en;
 
---
+++ src/split.rs
@@ -0,0 +1,175 @@
+use crate::Rect;
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub enum Div {
+    Px(f32),
+    // X(f32),
+    Ratio(f32),
+    Grow(f32),
+    // Pt(f32) <-- I REALLY want this
+}
+pub const GROW: Div = Div::Grow(1.0);
+impl Div {
+    pub fn px(&self) -> f32 {
+        match self {
+            Div::Px(px) => *px,
+            // Div::X(n) => x_px * n,
+            Div::Ratio(_) => 0.0, // This is the one smelly part of this API.
+            Div::Grow(_) => 0.0, // I experimented with a separate Dim class consisting of only X and Px, but it was less ergonomic.
+        }
+    }
+
+    fn px_final(&self, total_px: f32, leftover: f32, flex_total: f32) -> f32 {
+        match self {
+            Div::Px(px) => *px,
+            // Div::X(xs) => x_px * xs,
+            Div::Ratio(ratio) => ratio * total_px,
+            Div::Grow(flex) => {
+                if leftover > 0.0 {
+                    leftover * flex / flex_total
+                } else {
+                    0.0
+                }
+            }
+        }
+    }
+}
+
+fn leftover(divs: impl IntoIterator<Item = Div>, total_px: f32) -> f32 {
+    divs.into_iter().fold(total_px, |acc, div| match div {
+        Div::Px(px) => (acc - px),
+        // Div::X(x) => (acc - x_px * x),
+        Div::Ratio(ratio) => (acc - ratio * total_px),
+        Div::Grow(_) => acc,
+    })
+}
+
+fn flex_total(divs: impl IntoIterator<Item = Div>) -> f32 {
+    divs.into_iter().fold(0.0, |acc, div| match div {
+        Div::Grow(weight) => acc + weight,
+        _ => acc,
+    })
+}
+
+fn map_to_px<const N: usize>(divs: [Div; N], total_px: f32) -> [f32; N] {
+    let leftover = leftover(divs, total_px);
+    let flex_total = flex_total(divs);
+
+    divs.map(|div| div.px_final(total_px, leftover, flex_total))
+}
+
+fn map_to_px_iter(divs: impl IntoIterator<Item = Div> + Copy, total_px: f32) -> Vec<f32> {
+    let leftover = leftover(divs, total_px);
+    let flex_total = flex_total(divs);
+
+    divs.into_iter()
+        .map(|div| div.px_final(total_px, leftover, flex_total))
+        .collect()
+}
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub enum Align {
+    Start,
+    Center,
+    End,
+}
+
+impl Rect {
+    pub fn split_row<const N: usize>(&self, divs: [Div; N], align: Align) -> [Rect; N] {
+        let widths = map_to_px(divs, self.width());
+        let total = widths.iter().fold(0.0, |acc, width| acc + width);
+
+        let top = self.top();
+        let left = self.left();
+        let bottom = self.bottom();
+        let mut running_left = match align {
+            Align::Start => left,
+            Align::Center => left + (self.width() - total) / 2.0,
+            Align::End => left + (self.width() - total),
+        };
+
+        widths.map(|width| {
+            let left = running_left;
+            let right = left + width;
+            running_left = right;
+            Rect::from_top_right_bottom_left(top, right, bottom, left)
+        })
+    }
+
+    pub fn split_column<const N: usize>(&self, divs: [Div; N], align: Align) -> [Rect; N] {
+        let heights = map_to_px(divs, self.height());
+        let total = heights.iter().fold(0.0, |acc, height| acc + height);
+
+        let top = self.top();
+        let left = self.left();
+        let right = self.right();
+        let mut running_top = match align {
+            Align::Start => top,
+            Align::Center => top + (self.height() - total) / 2.0,
+            Align::End => top + (self.height() - total),
+        };
+
+        heights.map(|height| {
+            let top = running_top;
+            let bottom = top + height;
+            running_top = bottom;
+            Rect::from_top_right_bottom_left(top, right, bottom, left)
+        })
+    }
+
+    pub fn split_row_vec(
+        &self,
+        divs: impl IntoIterator<Item = Div> + Copy,
+        align: Align,
+    ) -> Vec<Rect> {
+        let widths = map_to_px_iter(divs, self.width());
+        let total = widths.iter().fold(0.0, |acc, width| acc + width);
+
+        let top = self.top();
+        let left = self.left();
+        let bottom = self.bottom();
+        let mut running_left = match align {
+            Align::Start => left,
+            Align::Center => left + (self.width() - total) / 2.0,
+            Align::End => left + (self.width() - total),
+        };
+
+        widths
+            .iter()
+            .map(|width| {
+                let left = running_left;
+                let right = left + width;
+                running_left = right;
+                Rect::from_top_right_bottom_left(top, right, bottom, left)
+            })
+            .collect()
+    }
+
+    pub fn split_column_vec(
+        &self,
+        divs: impl IntoIterator<Item = Div> + Copy,
+        align: Align,
+    ) -> Vec<Rect> {
+        let heights = map_to_px_iter(divs, self.height());
+        let total = heights.iter().fold(0.0, |acc, height| acc + height);
+
+        let top = self.top();
+        let left = self.left();
+        let right = self.right();
+        let mut running_top = match align {
+            Align::Start => top,
+            Align::Center => top + (self.height() - total) / 2.0,
+            Align::End => top + (self.height() - total),
+        };
+
+        heights
+            .iter()
+            .map(|height| {
+                let top = running_top;
+                let bottom = top + height;
+                running_top = bottom;
+                Rect::from_top_right_bottom_left(top, right, bottom, left)
+            })
+            .collect()
+    }
+}
